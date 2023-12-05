af94c1605c279ece2adbfa833d9a1217f4b91cfc
Louis Jeudy <louisjeudy1@gmail.com>
2023-12-05 09:34:31 +0000

    [EDIT] Factorisation deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,29 +13,27 @@ kubectl apply -f secret.yaml
 
 echo "-------------------- Deployment of Kafka -------------------- "
 
-kubectl apply -f kafka/k8s/services/kafka_service.yaml
-kubectl apply -f kafka/k8s/pods/kafka_pod.yaml
+kubectl apply -R -f kafka/k8s
 
 echo "-------------------- Deployment of Kowl -------------------- "
 
-kubectl apply -f kafka/kowl/kowl.yaml
-kubectl apply -f kafka/kowl/kowl_service.yaml
+kubectl apply -R -f kafka/kowl
 
 echo "-------------------- Deployment of Cassandra -------------------- "
 
-kubectl create configmap cassandra-script --from-file=cassandra/k8s/script/cassandra_schema.cql
-kubectl apply -f cassandra/k8s/volume/cassandra-persistent-volume.yaml
-kubectl apply -f cassandra/k8s/volume/cassandra-pvc.yaml
-kubectl apply -f cassandra/k8s/deployment/cassandra-load-keyspace-deployment.yaml
-kubectl apply -f cassandra/k8s/service/cassandra-service.yaml
-kubectl apply -f cassandra/k8s/deployment/cassandra-deployment.yaml
+kubectl create configmap cassandra-script --from-file=cassandra/k8s/script/cassandra_schema.cql --namespace=database
+kubectl apply -R -f cassandra/k8s/
 
 echo "-------------------- Deployment of Producer -------------------- "
 
-kubectl apply -f kafka/producer/k8s/configmaps/producer_configmap.yaml
-kubectl apply -f kafka/producer/k8s/deployments/producer_deploy.yaml
+kubectl apply -R -f kafka/producer/k8s
+
+echo "-------------------- Deployment of Consumer -------------------- "
+
+kubectl apply -R -f kafka/consumer/k8s
 
 echo "-------------------- Deployment of Prometheus -------------------- "
+
 kubectl create configmap prometheus-server-conf --from-file=./prometheus/configs
 kubectl apply -R -f prometheus/kube_state_metrics
 kubectl apply -R -f prometheus/node_exporter
@@ -43,6 +41,7 @@ kubectl apply -R -f prometheus/k8s
 kubectl apply -R -f prometheus/alertmanager
 
 echo "-------------------- Deployment of Grafana -------------------- "
+
 kubectl create configmap grafana-dashboards-files --from-file=./grafana/dashboards
 kubectl apply -R -f grafana/k8s
 
