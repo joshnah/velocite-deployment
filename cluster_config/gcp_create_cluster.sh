430518ea91888125f2946b2915726163ca2bb528
Viviane Qian <viviane.qian@grenoble-inp.org>
2024-01-17 11:52:13 +0000

    Remove cassandra-disk

--- cluster_config/gcp_create_cluster.sh
+++ cluster_config/gcp_create_cluster.sh
@@ -3,5 +3,3 @@
 gcloud config set compute/zone europe-west6-a
 
 gcloud container clusters create sdtd-cluster --num-nodes=4 --disk-size=50GB --quiet
-
-gcloud compute disks create cassandra-disk --size=20GB --type=pd-ssd --quiet
