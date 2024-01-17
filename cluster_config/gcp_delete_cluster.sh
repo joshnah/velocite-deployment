f7d51e1f48cd927014569039b916b8e7a1017360
Viviane Qian <viviane.qian@grenoble-inp.org>
2024-01-17 15:17:07 +0000

    Delete created PVC

--- cluster_config/gcp_delete_cluster.sh
+++ cluster_config/gcp_delete_cluster.sh
@@ -2,3 +2,6 @@
 
 gcloud config set compute/zone europe-west6-a
 gcloud container clusters delete sdtd-cluster --quiet
+
+# Delete all PVCs
+gcloud compute disks delete $(gcloud compute disks list --filter="name~'pvc-*'" --format "value(uri())") --quiet
