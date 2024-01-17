3935c89e160edad40d387eab572e26ea7c2979f3
Viviane Qian <viviane.qian@grenoble-inp.org>
2024-01-17 10:37:01 +0000

    Downsize nodes disk size

--- cluster_config/gcp_create_cluster.sh
+++ cluster_config/gcp_create_cluster.sh
@@ -2,6 +2,6 @@
 
 gcloud config set compute/zone europe-west6-a
 
-gcloud container clusters create sdtd-cluster --num-nodes=4 --quiet
+gcloud container clusters create sdtd-cluster --num-nodes=4 --disk-size=50GB --quiet
 
 gcloud compute disks create cassandra-disk --size=20GB --type=pd-ssd --quiet
