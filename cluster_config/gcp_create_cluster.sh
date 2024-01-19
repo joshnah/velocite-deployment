bb849ea76b1892156fb087ca7f48a880953b5fde
joshnah <tiendatamser296@gmail.com>
2024-01-19 09:44:58 +0100

    5 noeud

--- cluster_config/gcp_create_cluster.sh
+++ cluster_config/gcp_create_cluster.sh
@@ -2,4 +2,4 @@
 
 gcloud config set compute/zone europe-west6-a
 
-gcloud container clusters create sdtd-cluster --num-nodes=4 --disk-size=50GB --quiet
+gcloud container clusters create sdtd-cluster --num-nodes=5 --disk-size=50GB --quiet
